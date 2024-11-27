f8c82476c1af0f1ad9d021ac82107871e6e9f8de
minhtuyen260609 <tuyendo260906@gmail.com>
2024-11-27 21:49:30 +0700

    Add files via upload

---
+++ SS7-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int numbers[3]={1,2,3};
+	printf("%d\n", numbers[0]); 
+	printf("%d\n", numbers[1]); 
+	printf("%d\n", numbers[2]); 
+	printf("%d\n", sizeof(numbers)/ sizeof(int)); 
+	return 0; 
+} 
